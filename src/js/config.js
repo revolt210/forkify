1e25d9f94c99584aeb8e482b231c1a2dacb58303
Pasha <pashardenko@ukr.net>
2019-08-04 16:38:01 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+//49e58e8be80597aeabec9dae0b241bb0
+//2a4135f5bdd7f5f50f041e0a0497898b
+export const key = '49e58e8be80597aeabec9dae0b241bb0';
